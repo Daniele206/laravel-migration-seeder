5207f7f048ec36f3b852f7e1b0a268e10f26fb30
Daniele206 <danieledeieso2004@gmail.com>
2024-05-13 18:18:58 +0200

    [add] table structure

--- database/migrations/2024_05_13_152209_create_trains_table.php
+++ database/migrations/2024_05_13_152209_create_trains_table.php
@@ -13,6 +13,16 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('slug', 100)->unique();
+            $table->string('agency', 100);
+            $table->string('departure_station', 100);
+            $table->string('arrival_station', 100);
+            $table->string('departure_time', 5);
+            $table->string('Arrival time', 5);
+            $table->smallInteger('train_code');
+            $table->smallInteger('number_of_carriages')->unsigned();
+            $table->boolean('in_time')->default(true);
+            $table->boolean('deleted')->default(false);
             $table->timestamps();
         });
     }
